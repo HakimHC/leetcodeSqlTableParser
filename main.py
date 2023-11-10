3175b890e9c363547531db10953474d34c58344d
Hakim Ahmed <hakimahmed@MacBook-Pro.local>
2023-11-10 20:31:55 +0100

    Changed main to make connection to the db easier

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import os
 import sample_data
 from LcSqlParser import LcSqlParser
+from sqlalchemy import create_engine
+
+
+def connect_to_database():
+    host = os.getenv("DB_HOST")
+    database = os.getenv("DB_DATABASE")
+    port = os.getenv("DB_PORT")
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+
+    return create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
 
 def main():
-    mydb = LcSqlParser(sample_data.table_raw)
-    print(f"All the tables: {mydb.get_tables()}")
+    parser = LcSqlParser(sample_data.table_raw)
+    print(f"All the tables: {parser.get_tables()}")
 
-    for table in mydb.get_tables():
+    for table in parser.get_tables():
         print("Name:", table.name)
         print(table.df)
+
+    sql_cnx = connect_to_database()
+    parser.upload_to_database(sql_cnx)
 
 
 if __name__ == "__main__":
